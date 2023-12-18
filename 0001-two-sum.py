e38de5827ecbe8c33d119a8effc5891ba216735e
Briano Goestiawan <b@briano.io>
2023-12-18 18:22:59 +1100

    two-sum: not that slow

--- 0001-two-sum.py
+++ 0001-two-sum.py
@@ -16,10 +16,8 @@ class Solution:
 
 class SolutionSlow:
 	def twoSum(self, nums: list[int], target: int) -> list[int]:
-		for i in range(len(nums)):
-			for j in range(len(nums)):
-				if i == j:
-					continue
+		for i in range(len(nums)-1):
+			for j in range(i+1, len(nums)):
 				if nums[i] + nums[j] == target:
 					return [i, j]
 
